3cd9d86db2394f5c48ce0f41ef3375e7bf680330
amirabbas sadeghian <amir.abbas00@gmail.com>
2012-02-22 17:33:30 +0100

    Stu-paper matrix done

--- code.py
+++ code.py
@@ -1,7 +1,26 @@
 import inputdata
+import numpy
 data = inputdata.raw_scores
 
 def main():
-	for i 
-	
+	paperset=set()
+	papers=[]
+	stu=data.keys()
+	ratings=data.values()
+	for i in ratings:
+		temppaper=i.keys()
+		for j in temppaper:
+			paperset.add(j)
+	for i in paperset:
+		papers.append(i)
+	rates=numpy.ndarray((len(stu),len(papers)), dtype=float)
+
+	for i,j in data.iteritems():
+		for k,l in j.iteritems():
+			rates[stu.index(i)][papers.index(k)]=float(l)
+		
+	print stu
+	print papers
+	print rates
+
 main()
